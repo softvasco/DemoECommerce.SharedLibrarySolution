a41f21f508e8625d5e9f1f7469681f6e17a92420
Vasco Silva <sprvasco@hotmail.com>
2024-08-09 17:38:28 +0100

    implementation of ListenToOnlyApiGateway

---
+++ eCommerce.SharedLibrary/Middleware/ListenToOnlyApiGateway.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace eCommerce.SharedLibrary.Middleware
+{
+    public class ListenToOnlyApiGateway(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Extract specific header from the request
+            var signedHeader = context.Request.Headers["Api-Gateway"];
+
+
+            // NULL means, the request is not coming from the Api Geteway // 503 Service unavailable
+            if (signedHeader.FirstOrDefault() is null)
+            {
+                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                await context.Response.WriteAsync("Sorry, service if unavailable");
+                return;
+            }
+            else
+            {
+                await next(context);
+            }
+        }
+    }
+}
