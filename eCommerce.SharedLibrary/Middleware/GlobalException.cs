8400597256385731d51618b9575cc2731d005702
Vasco Silva <sprvasco@hotmail.com>
2024-08-09 16:18:55 +0100

    finishing GlobalException implementation

--- eCommerce.SharedLibrary/Middleware/GlobalException.cs
+++ eCommerce.SharedLibrary/Middleware/GlobalException.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using eCommerce.SharedLibrary.Logs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using System.Text.Json;
 
 namespace eCommerce.SharedLibrary.Middleware
 {
@@ -7,28 +10,75 @@ namespace eCommerce.SharedLibrary.Middleware
     {
         public async Task InvokeAsync(HttpContext context)
         {
-            //Declare variables 
+            //Declare default variables 
             string message = "sorry, internal server error occirred. Kindly try again";
-            int statusCOde = (int) HttpStatusCode.InternalServerError;
+            int statusCode = (int)HttpStatusCode.InternalServerError;
             string title = "Error";
 
             try
             {
                 await next(context);
 
-                //check if exception is too Many requests //429
+                //check if response is too Many requests //429
                 if (context.Response.StatusCode == StatusCodes.Status429TooManyRequests)
                 {
                     title = "Warning";
                     message = "Too many requets";
+                    statusCode = StatusCodes.Status429TooManyRequests;
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+
+
+                // if Response is UnAuthorized // 401 Status code
+                if (context.Response.StatusCode == StatusCodes.Status401Unauthorized)
+                {
+                    title = "Alert";
+                    message = "You are not authorized to access";
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+
+                // If Response is Forbidden  // 403 status code
+                if (context.Response.StatusCode == StatusCodes.Status403Forbidden)
+                {
+                    title = "Out of Access";
+                    message = "You are not allowed/required to access.";
+                    statusCode = StatusCodes.Status403Forbidden;
+                    await ModifyHeader(context, title, message, statusCode);
                 }
 
             }
             catch (Exception ex)
             {
-                
+                // Log Original Exceptions /File, Debugger, Console
+                LogException.LogExceptions(ex);
+
+                // check if exception is Timeout // 408 request timeout
+                if (ex is TaskCanceledException || ex is TimeoutException)
+                {
+                    title = "Out of time";
+                    message = "Request timeout .. try again";
+                    statusCode = StatusCodes.Status408RequestTimeout;
+                }
+
+                // IF exception is caugth 
+                //if none of the exceptions then do the default
+                await ModifyHeader(context, title, message, statusCode);
             }
 
+        }
+
+        private static async Task ModifyHeader(HttpContext context, string title, string message, int statusCode)
+        {
+            // display scary-free message to client
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonSerializer.Serialize(new ProblemDetails()
+            {
+                Detail = message,
+                Status = statusCode,
+                Title = title
+            }), CancellationToken.None);
+
+            return;
         }
     }
 }
