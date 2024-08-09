4c4ff7f1cac9b34ea22c00df1676a7e72b64e0f7
Vasco Silva <sprvasco@hotmail.com>
2024-08-09 17:59:48 +0100

    add SharedServiceContainer implementation

---
+++ eCommerce.SharedLibrary/DependencyInjection/SharedServiceContainer.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace eCommerce.SharedLibrary.DependencyInjection
+{
+    public static class SharedServiceContainer
+    {
+        public static IServiceCollection AddSharedServiceContainer<TContenxt>(this IServiceCollection services,
+            IConfiguration config, string fileName) where TContenxt : DbContext
+        {
+            // Add generic Database context
+            services.AddDbContext<TContenxt>(option => option.UseSqlServer(
+                config.GetConnectionString(fileName), sqlserverOption =>
+                sqlserverOption.EnableRetryOnFailure()
+                ));
+
+            //configure serilog logging
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(path: $"{fileName}-.text", restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {message:lj}{NewLine}{Exception}",
+                rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            // Add JWT authentication scheme
+            JWTAuthenticationScheme.AddJWTAuthenticationScheme(services, config);
+
+            return services;
+        }
+
+    }
+}
